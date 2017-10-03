e7be94556b644af9902e75a4a8a529bf7fa481fa
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-10-03 23:36:57 +0300

    Update Break.java

--- Ex6/src/Ex6/Break.java
+++ Ex6/src/Ex6/Break.java
@@ -9,7 +9,7 @@ public class Break {
         double G = 0, k = 0, n = 0;
         if (in.hasNextDouble()) {
             n = in.nextDouble();
-        } else {
+        } else {    // этот фрагмент надо перенести в конец
             System.out.println("Вы ввели не число!");
         }
         int p = 1;
@@ -19,6 +19,7 @@ public class Break {
             G = G + 1.0 / Math.pow((2.0 * k - 1.0), 2) * p;
             p = -p;
             // Как правильно реализовать 6 пункт б?
+            // мы это еще не проходили
             if (G <= e){
                 System.out.println(G);
                 break;
