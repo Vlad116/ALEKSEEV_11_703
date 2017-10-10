3395e2714e937504d4c857a28fedab4113b58274
Vlad116 <vladalekseev9@yandex.ru>
2017-10-10 08:29:34 +0300

    EX12,ex5b,ex7 - in ru.itis package , ex6b in EX_6... 12B,C,d will add during the day.

--- src/ru/itis/EX12V.java
+++ src/ru/itis/EX12V.java
@@ -1,7 +1,18 @@
 package ru.itis;
 
-public class EX12V {
-    public static void main (String[]args){
+import java.util.Scanner;
 
+public class EX12V {
+    public static void main (String[]args) {
+        System.out.println("Введите числа x и eps: ");
+        double S = 0, t = 1, eps, x;
+        int n = 1;
+        Scanner in = new Scanner(System.in);
+        Scanner ep = new Scanner(System.in);
+        x = in.nextDouble();
+        eps = ep.nextDouble();
+        while (Math.abs(t) > eps) {
+            int p = -1;
+        }
     }
 }
