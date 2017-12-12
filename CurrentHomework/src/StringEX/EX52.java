a05b183f136800a9a0b5a3b8b87e0e4f0fa052d4
Vbayrasheva <32329206+Vbayrasheva@users.noreply.github.com>
2017-12-13 00:19:36 +0300

    Update EX52.java

--- CurrentHomework/src/StringEX/EX52.java
+++ CurrentHomework/src/StringEX/EX52.java
@@ -1,4 +1,4 @@
-package StringEX;
+package StringEX;//-
 
 import java.util.Scanner;
 
@@ -30,3 +30,15 @@ public class EX52 {
     }
 }
 
+/*aaba
+  aba
+Exception in thread "main" java.lang.StringIndexOutOfBoundsException: String index out of range: 4
+	at java.lang.String.charAt(String.java:658)
+	at DZ622.EX52.main(EX52.java:16)
+	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+	at java.lang.reflect.Method.invoke(Method.java:498)
+	at com.intellij.rt.execution.application.AppMain.main(AppMain.java:147)
+
+Process finished with exit code 1*/
