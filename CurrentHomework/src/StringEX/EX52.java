2383973b62c9ec0d5b2efe3d3155c50328e9df8f
Vlad116 <vladalekseev9@yandex.ru>
2017-12-17 18:09:49 +0300

    fix string ex52

--- CurrentHomework/src/StringEX/EX52.java
+++ CurrentHomework/src/StringEX/EX52.java
@@ -1,44 +1,28 @@
-package StringEX;//-
+package StringEX;
 
 import java.util.Scanner;
 
 public class EX52 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s1 = sc.next();// Пример: s1 = blackcat,s2 = cat , s2 cодержиться в s1
-        String s2 = sc.next();
+    public static boolean Contains(String st1, String st2) {
         boolean f = false;
-        for (int i = 0; i < s1.length(); i++) {
-            int coincidence = 0;
-            if (s1.charAt(i) == s2.charAt(0)) {
-                coincidence = 1;
-                for (int j = 1; j < s2.length(); j++) {
-                    if (s1.charAt(i + j) != s2.charAt(j)) {
-                        break;
-                    }
-                    coincidence++;
-                }
-            }
-            if (coincidence == s2.length()) {
-                f = true;
+        for (int i = 0; i < st2.length() - st1.length() + 1 && !f; i++) {
+            for (int k = 0; k < st1.length(); k++) {
+                if (st1.charAt(k) != st2.charAt(k + i))
+                    break;
+                else if (k != 0 && st1.charAt(k) == st2.charAt(k + i))
+                    f = true;
             }
         }
-        if (f)
-            System.out.println("Строка s2 содержиться в s1");
+        return f;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        if (s1.length() < s2.length())
+            System.out.println(Contains(s1, s2));
         else
-            System.out.println("Строка s2 не содержиться в s1");
+            System.out.println(Contains(s2, s1));
     }
 }
-
-/*aaba
-  aba
-Exception in thread "main" java.lang.StringIndexOutOfBoundsException: String index out of range: 4
-	at java.lang.String.charAt(String.java:658)
-	at DZ622.EX52.main(EX52.java:16)
-	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
-	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
-	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
-	at java.lang.reflect.Method.invoke(Method.java:498)
-	at com.intellij.rt.execution.application.AppMain.main(AppMain.java:147)
-
-Process finished with exit code 1*/
